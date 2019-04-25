db07dc19378fe807f85a0ca77bcb8226d695781b
kingsmooth95 <srh0701@gmail.com>
2019-04-25 18:51:57 -0400

    Version 1.0.3

---
+++ server/app.js
@@ -0,0 +1,19 @@
+// Express.js server file (app)
+
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import loader from './loader';
+
+const app = express();
+
+// request logger with morgan
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer"'));
+
+// server static file
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+app.use('/', loader);
+
+// export module
+export default app;
